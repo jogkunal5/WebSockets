775fce0fdca207f9495c54ea91b9d23e1b496eb9
KJ <jogkunal5@gmail.com>
2021-08-10 10:51:14 +0530

    file download code

--- server.js
+++ server.js
@@ -6,6 +6,21 @@ s.on('connection', function(ws){
 		console.log("Received from client:", msg);
 		let data = "Hey.. I am server! I received your data :) now sending your data back ===>  "+ msg;
 		ws.send(data);
+		
+		// Read file and send to client for download
+		
+		console.log("Request received from client:", msg);
+
+		if (msg === "download") {
+			fs.readFile('./files/csvfile.csv',function(err,data){
+				if(err){
+					console.log(err);
+				}
+				//console.log(data);
+				ws.send(data,{binary:true});
+			});
+		}
+		
 	});	
 	
 	ws.on('close',function(){
